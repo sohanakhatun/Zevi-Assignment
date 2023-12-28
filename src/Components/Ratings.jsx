01d1b6e9d6e21b10bcdfa2b26d8fa597c2a9dd83
sohanakhatun <sohanakhatun16@gmail.com>
2023-12-28 15:19:23 +0530

    bug fix

--- src/Components/Ratings.jsx
+++ src/Components/Ratings.jsx
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import React from "react";
 import ReactStars from "react-stars";
 const Ratings = ({ ratings }) => {
@@ -10,16 +10,4 @@ const Ratings = ({ ratings }) => {
 };
 
 export default Ratings;
-=======
-import React from "react";
-import ReactStars from "react-stars";
-const Ratings = ({ ratings }) => {
-  return (
-    <div>
-      <ReactStars count={5} value={ratings}  size={24} color={"yellow"} />
-    </div>
-  );
-};
 
-export default Ratings;
->>>>>>> 06a5dc1926b9643d723c9e434186a4521f55fcf3
--- src/context/productContext.js
+++ src/context/productContext.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import React, { createContext, useContext } from "react";
 export const ProductContext = createContext({
   products: [],
@@ -10,16 +10,4 @@ export const ProductContextProvider = ProductContext.Provider;
 export default function useProducts() {
   return useContext(ProductContext);
 }
-=======
-import React, { createContext, useContext } from "react";
-export const ProductContext = createContext({
-  products: [],
-  setProducts: () => {},
-});
-export const ProductContextProvider = ProductContext.Provider;
 
-// custom hook
-export default function useProducts() {
-  return useContext(ProductContext);
-}
->>>>>>> 06a5dc1926b9643d723c9e434186a4521f55fcf3
